1de0fd3d0d2a3e65f8b189d90df9ac39ea10a857
robson.koji <robson.koji@gmail.com>
2020-11-26 00:51:58 -0300

    change b3_reference_data app to reference_data

--- b3_reference_data/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class B3ReferenceDataConfig(AppConfig):
-    name = 'b3_reference_data'
---
+++ reference_data/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ReferenceDataConfig(AppConfig):
+    name = 'reference_data'

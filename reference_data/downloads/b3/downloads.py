7e8c83e8c292bc5e6430fef5eb50c942682288df
robson.koji@gmail.com <Koji>
2021-04-04 19:42:12 -0300

    fix line chart

--- reference_data/downloads/b3/downloads.py
+++ reference_data/downloads/b3/downloads.py
@@ -186,6 +186,10 @@ class Aluguel(downloadMain, PdTableMixin):
         #self.pg_url = 'http://www.b3.com.br/pt_br/market-data-e-indices/servicos-de-dados/market-data/consultas/mercado-a-vista/termo/posicoes-em-aberto/'
         self.pg_url = 'http://www.b3.com.br/pt_br/produtos-e-servicos/emprestimo-de-ativos/renda-variavel/posicoes-em-aberto/renda-variavel-8AE490C9701B5B35017039842ACE1F91.htm?data=%s&f=0' % (self.today)
 
+PEGAR DAQUI, MAS SOH CONSEGUI UM A UM
+                        http://www.b3.com.br/pt_br/produtos-e-servicos/emprestimo-de-ativos/renda-variavel/posicoes-em-aberto/
+
+
     def get_pg_data(self):
         # import pdb; pdb.set_trace()
         r = self.req_session.get(self.pg_url)

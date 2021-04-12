5df76a67ff3146b0368566cfef2d48d567425af6
robson.koji@gmail.com <Koji>
2021-04-11 21:56:50 -0300

    add new stock code to get_stock_prices.py

--- tasks/get_stocks_prices.py
+++ tasks/get_stocks_prices.py
@@ -19,6 +19,7 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_b3_ir_calc.settings')
 django.setup()
 from stock_price.models import StockPrice
 from stock_price.stock_price import get_stocks, get_price, read_csvs_dir
+from corporate_events.models import CorporateEvent
 
 def save_stocks(stock, price, hour):
     stock_obj, created = StockPrice.objects.get_or_create(stock=stock)
@@ -31,11 +32,13 @@ def save_stocks(stock, price, hour):
 if __name__ == "__main__":
     stocks = read_csvs_dir()
     stocks_db =  StockPrice.objects.values_list('stock', flat=True)
+    stocks_new_code = CorporateEvent.objects.values_list('asset_code_new', flat=True)
+
     stock_price = defaultdict()
 
     # Juntando acoes dos arquivos csv (b3 CEI), com o que estah no banco.
     # No banco pode ter acoes do 11 recomenda.
-    stocks_list = list(set(stocks) | set(stocks_db))
+    stocks_list = list(set(stocks) | set(stocks_db)) | set(stocks_new_code))
     # import pdb; pdb.set_trace()
     for stock in stocks_list:
         try:

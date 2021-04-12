f291a9c191dce76186b5d1bbdb58f59431c259bf
robson.koji@gmail.com <Koji>
2021-04-11 21:56:50 -0300

    add new stock code to get_stock_prices.py

--- stock_price/stock_price.py
+++ stock_price/stock_price.py
@@ -46,7 +46,7 @@ def get_price(stock):
 
     url = 'https://finance.yahoo.com/quote/%s.sa' % (stock)
     try:
-        print(url)
+        # print(url)
         page = urlopen(url)
         soup = bs4.BeautifulSoup(page,'html.parser')
         price = soup.find('div',{'class': 'My(6px) Pos(r) smartphone_Mt(6px)'}).find('span').text

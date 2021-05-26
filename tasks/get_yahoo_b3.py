bb5a3bc94d2e7d2d6e4773a8f9138f8051db23b5
robson.koji@gmail.com <Koji>
2021-05-25 22:01:06 -0300

    change interval

--- tasks/get_yahoo_b3.py
+++ tasks/get_yahoo_b3.py
@@ -36,7 +36,7 @@ def get_data():
             # fetch data by interval (including intraday if period < 60 days)
             # valid intervals: 1m,2m,5m,15m,30m,60m,90m,1h,1d,5d,1wk,1mo,3mo
             # (optional, default is '1d')
-            interval = "5m",
+            interval = "15m",
 
             # group by ticker (to access via data['SPY'])
             # (optional, default is 'column')

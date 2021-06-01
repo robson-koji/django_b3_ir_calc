36ed770c0c4821959017b97689a4da4d715a0c44
robson.koji@gmail.com <Koji>
2021-06-01 12:12:46 -0300

    change alert admin fields

--- alerts/admin.py
+++ alerts/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(MmAlert)
+
+class MmAlertAdmin(admin.ModelAdmin):
+    fields = ['stock', 'timeframe', 'smm', 'emm', 'active']
+
+    def save_model(self, request, obj, form, change):
+        obj.user = request.user
+        super().save_model(request, obj, form, change)
+
+admin.site.register(MmAlert, MmAlertAdmin)

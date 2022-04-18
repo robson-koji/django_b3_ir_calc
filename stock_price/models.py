901e294dca3f201651b92759509351acbef46c76
koji <robson.koji@gmail.com>
2022-04-17 21:55:23 -0300

    add stock name to fit broker information

--- stock_price/models.py
+++ stock_price/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class StockPrice(models.Model):
     stock = models.CharField(unique=True, max_length=15)
+    name = models.CharField(max_length=256, null=True, blank=True)
     sector = models.CharField(max_length=256, null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     hour = models.CharField(max_length=60, null=True, blank=True) # Time provided by Yahoo
